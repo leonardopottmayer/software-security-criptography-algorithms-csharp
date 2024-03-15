f17429ead4f79fb80c37b492a22457aa29fdf457
leonardopottmayer <pottmayerleonardo@gmail.com>
2024-03-14 23:06:53 -0300

    Added logs to console

--- SoftwareSecurity/Program.cs
+++ SoftwareSecurity/Program.cs
@@ -13,23 +13,48 @@ namespace SoftwareSecurity
 
         public static void TestVigenereCipher()
         {
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine($"Executing {nameof(VigenereCipher)}");
+            Console.WriteLine("--------------------------------------------------");
+
             var vigenereCipher = new VigenereCipher();
 
             string textToEncrypt = "THEY DRINK 1/5 THE TEA";
             string encryptionKey = "DUH";
 
+            Console.WriteLine($"Text to be encrypted: {textToEncrypt}");
+            Console.WriteLine($"Encryption key: {encryptionKey}");
+
             string encryptResult = vigenereCipher.Encrypt(textToEncrypt, encryptionKey);
-            string descryptResult = vigenereCipher.Decrypt(encryptResult, encryptionKey);
+            string decryptResult = vigenereCipher.Decrypt(encryptResult, encryptionKey);
+
+            Console.WriteLine($"Encrypted text: {encryptResult}");
+            Console.WriteLine($"Decrypted text: {decryptResult}");
+
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine();
         }
 
         public static void TestCaesarCipher()
         {
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine($"Executing {nameof(CaesarCipher)}");
+            Console.WriteLine("--------------------------------------------------");
+
             var caesarCipher = new CaesarCipher();
 
             string textToEncrypt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%¨&*()-_=+{[}]\\|;:'\",<.>/?";
 
+            Console.WriteLine($"Text to be encrypted: {textToEncrypt}");
+
             string encryptResult = caesarCipher.Encrypt(textToEncrypt);
             string decryptResult = caesarCipher.Decrypt(encryptResult);
+
+            Console.WriteLine($"Encrypted text: {encryptResult}");
+            Console.WriteLine($"Decrypted text: {decryptResult}");
+
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine();
         }
     }
 }
